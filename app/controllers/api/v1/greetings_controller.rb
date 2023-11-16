28200eb0052a40259a9ccaccc2e82e9ed076e690
mariegrace <mariegracebmg@gmail.com>
2023-11-16 19:23:43 +0200

    Chnage content to message

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
   def random
     @random_greeting = Greeting.order('RANDOM()').first
-    render json: { content: @random_greeting.message }
+    render json: { message: @random_greeting.message }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_13_164546) do
   enable_extension "plpgsql"
 
   create_table "greetings", force: :cascade do |t|
-    t.string "content"
+    t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
